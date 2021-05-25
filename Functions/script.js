7044e3a0199139e9b86fa3d297caaeb4a6850563
sboogaard <76215360+sboogaard@users.noreply.github.com>
2021-05-25 15:53:09 +0200

    Add files via upload

---
+++ Functions/script.js
@@ -0,0 +1,41 @@
+const number = 3;
+
+// 1 dough is good for 4 pizza's
+const makePizzaDough =  function () {
+    console.log('Mix yeast and luke warm water')
+    console.log('Add flour to bowl')
+    console.log('Mix flour and yeast solution')
+    console.log('Add salt')
+    console.log('Stir until mixed')
+    console.log('Leave for 2 hours')
+};
+
+const makePizza = function () {
+    console.log('Preheat oven')
+    console.log('Roll out dough')
+    console.log('Do pizza-maker magic')
+    console.log('Add tomato sauce')
+    console.log('Add cheese')
+    console.log('*more cheese*')
+    console.log('Add meat')
+    console.log('Into the oven')
+};
+
+const makeSushi = function () {
+    console.log('Cook rice')
+    console.log('Slice tuna into thin strips')
+    console.log('Slice advocado into strips')
+    console.log('Drain rice')
+    console.log('Place ingredients on seaweed')
+    console.log('Roll')
+    console.log('Slice sushi roll into pieces')
+}
+
+makePizzaDough();
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+makePizza();
+makePizza();
