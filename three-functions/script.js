c6d656a278ff46b69783cf036dc17545c6450e22
sboogaard <76215360+sboogaard@users.noreply.github.com>
2021-05-26 14:27:37 +0200

    Add files via upload

---
+++ three-functions/script.js
@@ -0,0 +1,31 @@
+function square1(number1){
+    sum1 = (number1 * number1);
+    sum2 = (sum1 + sum1);
+    sum3 = (sum2 * sum2);
+    console.log(sum3);
+    return(sum3);
+    
+};
+square1(2);
+
+
+const square2 = function(number2) {
+    sum1 = (number2 * number2);
+    sum2 = (sum1 + sum1);
+    sum3 = (sum2 * sum2);
+    console.log(sum3);
+    return(sum3);
+    
+
+}
+square2(3);
+
+const square3 = (number3) => {
+    sum1 = (number3 * number3);
+    sum2 = (sum1 + sum1);
+    sum3 = (sum2 * sum2);
+    console.log(sum3);
+    return(sum3);
+
+};
+square3(4);
