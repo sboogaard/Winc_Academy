e4646c1cac9c44bb3dab065aa22eae0f9ec9eaaf
sboogaard <76215360+sboogaard@users.noreply.github.com>
2021-06-01 20:30:25 +0200

    Add files via upload

--- calling-function/script.js
+++ calling-function/script.js
@@ -1,35 +1,59 @@
-// const scream = () => {
-//     document.write("Aaaaaa!!!")
-// };
-
-// scream();
-
-// const help = () => {
-//     scream();
-// };
-
-// help();
-
-const ageChecker = () => {
-    const age = 19;
+const ageChecker = (age) => {
     if 
-        (age >= 18)
-        (ageChecker = true)
-        console.log(ageChecker);
-    else
-        console.log(ageChecker)
-    
+        (age >= 18){
+            return true;
+        }
+    else {
+        return false;
+    };
 }
 
-const ageVerify = () => {
-    if 
-        (ageChecker = true)
-        console.log("Come in!")
-    else
-        console.log("Go away!")
-    
-}
+const ageVerify = (age) => {
+    if (ageChecker(age)){
+        return "Hi there";
+    } else {
+        return "NO!"
+    }    
+};
 
-ageChecker();
-ageVerify();
+console.log(ageVerify(65))
+console.log(ageVerify(10))
 
+// Short
+
+const isAdultShort = age => age >= 18;
+
+const greetShort = age => (isAdult(age) ? "Hello there" : "Hey kiddo");
+
+console.log(greetShort(65)); 
+console.log(greetShort(10)); 
+
+//VAT 1
+
+const calculateVAT = function(basePrice, VATPercentage) {
+    return basePrice * (VATPercentage / 100);
+};
+
+const calculatePriceIncludingVAT = function(basePrice, VATPercentage) {
+    const VAT = calculateVAT(basePrice, VATPercentage);
+    return basePrice + VAT;
+};
+
+console.log(calculatePriceIncludingVAT(1000, 21)); // 1210
+console.log(calculatePriceIncludingVAT(2, 9)); // 2.18
+
+// VAT 2
+
+const calculateBasePrice = function(priceIncludingVAT, VATPercentage) {
+    const basePrice = priceIncludingVAT / ((100 + VATPercentage) / 100);
+    return basePrice;
+};
+
+const calculateBasePriceAndVAT = function(priceIncludingVAT, VATPercentage) {
+    const basePrice = calculateBasePrice(priceIncludingVAT, VATPercentage);
+    const VAT = priceIncludingVAT - basePrice;
+    return [basePrice, VAT];
+};
+
+console.log(calculateBasePriceAndVAT(1210, 21)); // [1000, 210]
+console.log(calculateBasePriceAndVAT(2.18, 9)); // [2, 0.18]
