d469cf49ddeaa67f755c522fe92b703bc0981fb5
sboogaard <76215360+sboogaard@users.noreply.github.com>
2021-06-01 20:18:57 +0200

    Add files via upload

---
+++ calling-function/script.js
@@ -0,0 +1,35 @@
+// const scream = () => {
+//     document.write("Aaaaaa!!!")
+// };
+
+// scream();
+
+// const help = () => {
+//     scream();
+// };
+
+// help();
+
+const ageChecker = () => {
+    const age = 19;
+    if 
+        (age >= 18)
+        (ageChecker = true)
+        console.log(ageChecker);
+    else
+        console.log(ageChecker)
+    
+}
+
+const ageVerify = () => {
+    if 
+        (ageChecker = true)
+        console.log("Come in!")
+    else
+        console.log("Go away!")
+    
+}
+
+ageChecker();
+ageVerify();
+
